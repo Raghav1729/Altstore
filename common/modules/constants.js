1ef22113fec02af15efca46274f09e97cf7156b2
Raghav Mishra <114184124+Raghav1729@users.noreply.github.com>
2024-10-09 10:08:18 +0530

    Update constants.js

--- common/modules/constants.js
+++ common/modules/constants.js
@@ -14,16 +14,7 @@ export const sourceURL = urlSearchParams.get('source')?.replaceAll("+", "%2B");
 export const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 
 export const sources = [
-    "https://raw.githubusercontent.com/arichornlover/arichornlover.github.io/main/apps.json",
-    "https://raw.githubusercontent.com/arichornlover/arichornlover.github.io/main/apps2.json",
-    "https://raw.githubusercontent.com/arichornlover/arichornlover.github.io/main/apps-legacy.json",
-    "https://therealfoxster.github.io/altsource/apps.json",
-    "https://qnblackcat.github.io/AltStore/apps.json",
-    "https://quarksources.github.io/dist/quantumsource.min.json",
-    "https://quarksources.github.io/dist/quantumsource%2B%2B.min.json",
-    "https://alt.getutm.app",
-    "https://flyinghead.github.io/flycast-builds/altstore.json",
-    "https://provenance-emu.com/apps.json"
+    "https://raw.githubusercontent.com/Raghav1729/altstore/refs/heads/master/altsore.json"
 ];
 
 export const entitlements = {
