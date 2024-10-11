f2d2fcb4c349ccd08b290db484624a6a1bccd1f6
Raghav Mishra <114184124+Raghav1729@users.noreply.github.com>
2024-10-11 13:34:45 +0530

    Update update_apps_json.py

--- update_apps_json.py
+++ update_apps_json.py
@@ -60,7 +60,34 @@ def create_app_data(repo, releases):
                 for i in range(1, 7)
             ],
             "localizedDescription": "Watch videos, subscribe to channels, and access personalized content with the enhanced YouTube experience.",
-            "subtitle": "Watch, create, and share your favorite content."           
+            "subtitle": "Watch, create, and share your favorite content.",
+            "appPermissions": {
+                "entitlements": [
+                    {"name": "com.apple.security.application-groups"},
+                    {"name": "com.apple.developer.associated-domains"},
+                    {"name": "com.apple.developer.coremedia.allow-alternate-video-decoder-selection"},
+                    {"name": "com.apple.developer.usernotifications.time-sensitive"},
+                    {"name": "get-task-allow"},
+                    {"name": "com.apple.developer.device-information.user-assigned-device-name"},
+                    {"name": "com.apple.developer.group-session"},
+                    {"name": "keychain-access-groups"},
+                    {"name": "com.apple.developer.networking.multicast"},
+                    {"name": "aps-environment"},
+                    {"name": "com.apple.developer.siri"},
+                    {"name": "com.apple.developer.networking.wifi-info"}
+                ],
+                "privacy": [
+                    {"name": "AppleMusic", "usageDescription": "Allows adding audio files to your videos."},
+                    {"name": "BluetoothPeripheral", "usageDescription": "Access to Bluetooth is required to scan for nearby Cast devices."},
+                    {"name": "Camera", "usageDescription": "Allows you to create videos using the app."},
+                    {"name": "Contacts", "usageDescription": "Contacts are sent to YouTube servers to help find friends."},
+                    {"name": "LocalNetwork", "usageDescription": "Access to your network enables discovering and connecting to devices."},
+                    {"name": "LocationWhenInUse", "usageDescription": "Attach location information to videos and improve recommendations."},
+                    {"name": "Microphone", "usageDescription": "Allows including audio in your videos and voice search."},
+                    {"name": "PhotoLibrary", "usageDescription": "Upload media you've already created."},
+                    {"name": "UserTracking", "usageDescription": "Tracks user activity for personalized content."}
+                ]
+            }
         })
     
     elif repo == "Raghav1729/BHTwitter":
@@ -89,33 +116,36 @@ def create_app_data(repo, releases):
                     {"name": "com.apple.developer.networking.wifi-info"}
                 ],
                 "privacy": [
-                    {"name": "BluetoothAlways", "usageDescription": "Bluetooth access is used for connecting to external devices."},
-                    {"name": "Calendars", "usageDescription": "Access to calendars is required for scheduling and event management."},
-                    {"name": "Camera", "usageDescription": "Camera access is used for sharing images."},
-                    {"name": "Contacts", "usageDescription": "Twitter accesses your contacts to connect you with friends."},
-                    {"name": "FaceID", "usageDescription": "Face ID is required for secure logins."},
-                    {"name": "LocalNetwork", "usageDescription": "Access to your network allows Twitter to discover and connect to devices."},
-                    {"name": "Location", "usageDescription": "Location data is used to provide personalized experiences."},
-                    {"name": "LocationWhenInUse", "usageDescription": "Allows Twitter to access your location when using the app."},
-                    {"name": "Microphone", "usageDescription": "Microphone access is needed for voice tweets."},
-                    {"name": "PhotoLibraryAdd", "usageDescription": "Allows the app to add photos to your library."},
-                    {"name": "PhotoLibrary", "usageDescription": "Access to your photo library is required to upload images."},
-                    {"name": "SpeechRecognition", "usageDescription": "Allows the app to recognize speech."},
-                    {"name": "UserTracking", "usageDescription": "This app requires access to track user activity for personalized content."}
+                    {"name": "BluetoothAlways", "usageDescription": "Connects to external devices using Bluetooth."},
+                    {"name": "Calendars", "usageDescription": "Access calendars for scheduling and event management."},
+                    {"name": "Camera", "usageDescription": "Share images using camera access."},
+                    {"name": "Contacts", "usageDescription": "Connects you with friends using your contacts."},
+                    {"name": "FaceID", "usageDescription": "Required for secure logins."},
+                    {"name": "LocalNetwork", "usageDescription": "Discovers and connects to devices on your network."},
+                    {"name": "Location", "usageDescription": "Provides personalized experiences using location data."},
+                    {"name": "LocationWhenInUse", "usageDescription": "Accesses your location while using the app."},
+                    {"name": "Microphone", "usageDescription": "Needed for voice tweets."},
+                    {"name": "PhotoLibraryAdd", "usageDescription": "Adds photos to your library."},
+                    {"name": "PhotoLibrary", "usageDescription": "Uploads images from your photo library."},
+                    {"name": "SpeechRecognition", "usageDescription": "Recognizes speech for enhanced user experience."},
+                    {"name": "UserTracking", "usageDescription": "Tracks user activity for personalized content."}
                 ]
             }
         })
 
     # Add the releases to the app data
     for release in releases:
-        version_info = {
-            "version": release["tag_name"].lstrip("v").split('-')[1] if repo == "Raghav1729/BHTwitter" else release["tag_name"].lstrip("v").split('-')[0],
-            "date": release["published_at"],
-            "localizedDescription": release.get("body", ""),  # Use get to avoid KeyError
-            "downloadURL": release["assets"][0]["browser_download_url"],
-            "size": release["assets"][0]["size"]
-        }
-        app_data["versions"].append(version_info)
+        if release.get("assets"):
+            version_info = {
+                "version": release["tag_name"].lstrip("v").split('-')[1] if repo == "Raghav1729/BHTwitter" else release["tag_name"].lstrip("v").split('-')[0],
+                "date": release["published_at"],
+                "localizedDescription": release.get("body", ""),  # Use get to avoid KeyError
+                "downloadURL": release["assets"][0]["browser_download_url"],
+                "size": release["assets"][0]["size"]
+            }
+            app_data["versions"].append(version_info)
+        else:
+            print(f"No assets found for release: {release['tag_name']}")
 
     return app_data
 
